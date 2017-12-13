fe619856afb069d98e547396b831418116aee90a
zhouyq <zhouyq@goodrain.com>
2017-12-13 19:23:07 +0800

    [ADD] 添加nginx和php配置文件

--- debian/jessie/docker-entrypoint.sh
+++ debian/jessie/docker-entrypoint.sh
@@ -2,12 +2,23 @@
 
 [[ $DEBUG ]] && set -x
 
+function start-php-fpm(){
+  echo -e "Start php-fpm in background."
+  php-fpm &
+}
+
+function start-nginx(){
+  echo -e "Start nginx in foreground...\n"
+  nginx -g 'daemon off;'
+}
+
 case $1 in
 "debug")
   /bin/bash;;
 "version")
-  echo "PHP:$PHP_VERSION"
-  echo "Nginx: $APACHE_VERSION";;
-*)
-  exec $@
+  echo "PHP: $PHP_VERSION"
+  echo "Nginx: $NGINX_VERSION";;
+"start")
+  start-php-fpm
+  start-nginx;;
 esac
